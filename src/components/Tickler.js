085b3c29c264f1d586df4319cbbbf25db0b8c890
hoonkim <hoonk9387@gmail.com>
2022-06-03 21:50:22 -0500

    Added onClick event listener.

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -4,8 +4,16 @@ function Tickler() {
   function tickle() {
     console.log("Teehee!");
   }
+  // Return JSX button.
+  /* Can only attach event listeners to DOM elements
+     but not on React components. */
+  // Add prop onClick event listener and pass callback function.
+  // Passing function reference tickle as prop value.
+  return <button onClick={tickle}>Tickle me!</button>;
 
-  return <button>Tickle me!</button>;
+  // Inline event handler using arrow function to 
+  // pass to event listener.
+  // return <button onClick={() => console.log("Teeeheeee!!!")}>Tickle Me!</button>
 }
 
 export default Tickler;
